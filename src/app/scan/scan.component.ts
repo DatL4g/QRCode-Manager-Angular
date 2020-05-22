403d5d0ea0a821087644333b5f0711ecead9d40c
DATL4G <jeff.retz08@gmail.com>
2020-05-23 00:05:10 +0200

    fixed qr-scanner libary issue

--- src/app/scan/scan.component.ts
+++ src/app/scan/scan.component.ts
@@ -1,5 +1,5 @@
 import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import QRScanner from 'qr-scanner';
+import QRScanner from './../../assets/javascript/qr-scanner.min.js';
 import * as DetectRTC from 'detectrtc';
 import * as global from './../../environments/global';
 import {BreakpointObserver} from "@angular/cdk/layout";
@@ -9,25 +9,31 @@ import {BreakpointObserver} from "@angular/cdk/layout";
   templateUrl: './scan.component.html',
   styleUrls: ['./scan.component.scss']
 })
-export class ScanComponent implements OnInit {
+export class ScanComponent implements OnInit, AfterViewInit {
 
   public globals: global.Globals;
+  public qrScanner: QRScanner;
 
-  @ViewChild('videoElement') videoElement: ElementRef
+  @ViewChild('videoInput') videoInput: ElementRef;
 
   constructor(private breakpointObserver: BreakpointObserver) {
     this.globals = new global.Globals(breakpointObserver);
   }
 
   ngOnInit(): void {
+    QRScanner.WORKER_PATH = './../../assets/javascript/qr-scanner-worker.min.js';
+  }
+
+  ngAfterViewInit(): void {
+    this.qrScanner = new QRScanner(this.videoInput.nativeElement, result => console.log('decode qr code: ', result));
+    this.qrScanner.setInversionMode("both");
+
     if (DetectRTC.hasWebcam) {
-      const qrScanner = new QRScanner(this.videoElement.nativeElement, result => console.log('decode qr code: ', result));
-      qrScanner.setInversionMode("both");
-      qrScanner.start();
-      console.log('Has Webcam')
+      this.qrScanner.start();
+      console.log('Has Webcam');
       console.log('Scanner started')
     } else {
-      console.log('No Webcam')
+      console.log('No Webcam');
     }
   }
 
